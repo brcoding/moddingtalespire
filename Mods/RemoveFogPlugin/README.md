f727ba6b74de1b252388574fcbbbb419c8adbbf4
h3lls <savagesoul@gmail.com>
2020-05-05 01:53:18 -0700

    Added RemoveFogPlugin

---
+++ Mods/RemoveFogPlugin/README.md
@@ -0,0 +1,28 @@
+# Remove Fog Plugin
+
+This is a plugin for TaleSpire using BepInEx.
+
+
+## Install
+
+Go to the releases folder and download the latest and extract to the contents of your TaleSpire game folder.
+
+## Usage
+
+To toggle fog press the __```g```__ key.
+
+## How to Compile / Modify
+
+Open ```RemoveFogPlugin.sln``` in Visual Studio.
+
+You will need to add references to:
+
+```
+* BepInEx.dll  (Download from the BepInEx project.)
+* UnityEngine.InputLegacyModule.dll (found in Steam\steamapps\common\TaleSpire\TaleSpire_Data\Managed)
+* Unity.Postprocessing.Runtime.dll
+```
+
+Build the project.
+
+Browse to the newly created ```bin/Debug``` or ```bin/Release``` folders and copy the ```RemoveFogPlugin.dll``` to ```Steam\steamapps\common\TaleSpire\BepInEx\plugins```
---
+++ Mods/RemoveFogPlugin/RemoveFogPlugin/RemoveFog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace RemoveFogPlugin
+{
+    [BepInPlugin("org.d20armyknife.plugins.removefog", "Remove Fog Plug-In", "1.0.0.0")]
+    [BepInProcess("TaleSpire.exe")]
+    public class DefogIt: BaseUnityPlugin
+    {
+
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Remove Fog Plug-in loaded");
+        }
+
+        private void ToggleFog()
+        {
+            this.fogEnabled = !this.fogEnabled;
+            RenderSettings.fog = this.fogEnabled;
+            var postProcessLayer = Camera.main.GetComponent<PostProcessLayer>();
+            postProcessLayer.fog.enabled = this.fogEnabled;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.G))
+            {
+                ToggleFog();
+            }
+        }
+
+        private bool fogEnabled = true;
+    }
+}
