a50fe308c0b417189397ef2469c221d1fc198a1a
h3lls <savagesoul@gmail.com>
2020-05-05 20:52:12 -0700

    Updated to include control+shift+mousewheel

--- Mods/RForRotate/RForRotatePlugin/RForRotate.cs
+++ Mods/RForRotate/RForRotatePlugin/RForRotate.cs
@@ -15,37 +15,53 @@ namespace RForRotate
             UnityEngine.Debug.Log("R For Rotate Plug-in loaded");
         }
         
-        private void RotateSelected()
+        private void RotateSelected(double amount)
         {
-            var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
-            if (SingletonBehaviour<BoardToolManager>.HasInstance && (SingletonBehaviour<BoardToolManager>.Instance.IsCurrentTool<SingleBuilderBoardTool>())) {
-                var btm = (SingleBuilderBoardTool)SingletonBehaviour<SingleBuilderBoardTool>.Instance;
-                var angle = (float)btm.GetType().GetField("_angle", flags).GetValue(btm);
-                angle = (float)(((double)angle + 90.0) % 360.0);
-                // After adjusting don't forget to push it back to TaleSpire so they can keep track of it.
-                btm.GetType().GetField("_angle", flags).SetValue(btm, angle);
-                var selectedAsset = (TilePreviewBoardAsset)btm.GetType().GetField("_selectedTileBoardAsset", flags).GetValue(btm);
-                selectedAsset.Rotate(angle);
+            try
+            {
+                // We have to first grab the CameraController._targetZoomLerpValue to disable the movement when our rotate is happening. This is the only way to prevent zooming while we are rotating
+                var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
+                if (SingletonBehaviour<BoardToolManager>.HasInstance && (SingletonBehaviour<BoardToolManager>.Instance.IsCurrentTool<SingleBuilderBoardTool>()))
+                {
+                    var btm = (SingleBuilderBoardTool)SingletonBehaviour<SingleBuilderBoardTool>.Instance;
+                    float angle = (float)btm.GetType().GetField("_angle", flags).GetValue(btm);
+                    angle = (float)(((double)angle + amount) % 360.0);
+                    
+                    // After adjusting don't forget to push it back to TaleSpire so they can keep track of it.
+                    btm.GetType().GetField("_angle", flags).SetValue(btm, angle);
+                    var selectedAsset = (TilePreviewBoardAsset)btm.GetType().GetField("_selectedTileBoardAsset", flags).GetValue(btm);
+                    selectedAsset.Rotate(angle);
+
+                    // Next we need to counter the heightPlaneOffset that is set in the CallUpdate when left shift is pressed
+                    float heightPlaneOffset = (float)btm.GetType().GetField("heightPlaneOffset", flags).GetValue(btm);
+                    heightPlaneOffset -= (float)((double)Input.mouseScrollDelta.y * (double)Time.deltaTime * 8.0);
+                    btm.GetType().GetField("heightPlaneOffset", flags).SetValue(btm, heightPlaneOffset);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.Log("Crash in r for rotate Plugin");
+                UnityEngine.Debug.Log(ex.Message);
+                UnityEngine.Debug.Log(ex.StackTrace);
+                UnityEngine.Debug.Log(ex.InnerException);
+                UnityEngine.Debug.Log(ex.Source);
             }
         }
-       
+
 
         void Update()
         {
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                    RotateSelected(90.0);
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                RotateSelected(-90.0);
+            }
             if (Input.GetKeyUp(KeyCode.R))
             {
-                try
-                {
-                    RotateSelected();
-                }
-                catch (System.Exception ex)
-                {
-                    UnityEngine.Debug.Log("Crash in r for rotate Plugin");
-                    UnityEngine.Debug.Log(ex.Message);
-                    UnityEngine.Debug.Log(ex.StackTrace);
-                    UnityEngine.Debug.Log(ex.InnerException);
-                    UnityEngine.Debug.Log(ex.Source);
-                }
+                RotateSelected(90.0);
             }
         }
 
