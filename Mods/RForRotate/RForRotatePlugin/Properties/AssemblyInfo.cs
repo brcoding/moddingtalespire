be45b5967f092eaf0cb93a4691a124e3ba8f5a97
h3lls <savagesoul@gmail.com>
2020-05-07 17:38:29 -0700

    Added slab handling for RForRotate
    Added injection flag

--- Mods/RForRotate/RForRotatePlugin/Properties/AssemblyInfo.cs
+++ Mods/RForRotate/RForRotatePlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RemoveDoFPlugin")]
+[assembly: AssemblyTitle("RForRotatePlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("D20ArmyKnife")]
-[assembly: AssemblyProduct("RemoveDoFPlugin")]
+[assembly: AssemblyProduct("RForRotatePlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
